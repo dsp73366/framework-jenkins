8e449a4367ed34dc695a1d5877e46207acde26d8
Lenovo <Lenovo@LAPTOP-LU0D8EQR>
2024-10-07 16:59:19 +0530

    push1

---
+++ frameworkJenkins/src/test/java/contactTest/tocreatecontact.java
@@ -0,0 +1,9 @@
+package contactTest;
+
+public class tocreatecontact {
+
+	public static void main(String[] args) {
+System.out.println("created successfully1");
+	}
+
+}
---
+++ frameworkJenkins/src/test/java/organizationTest/tocreateorganisation.java
@@ -0,0 +1,9 @@
+package organizationTest;
+
+public class tocreateorganisation {
+
+	public static void main(String[] args) {
+System.out.println("created successfully");
+	}
+
+}
